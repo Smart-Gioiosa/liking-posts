20e980c4d8ce7e2e29db65c20c29a37de3a8a7cd
Antonino <antoninoscaffidi@gmail.com>
2017-07-14 11:01:18 +0200

    Settin a page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Liking Postsand Photos like Facebbok and Twitter").join(" - ")
+      end
+    end
+  end
 end
